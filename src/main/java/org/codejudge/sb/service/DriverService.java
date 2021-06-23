7dfb0ce2702e1268ce7a68f6e6fb34da7a47a157
Shyam Mohan <shyam615shiva@gmail.com>
2021-06-23 17:14:31 +0530

    Add files via upload

--- src/main/java/org/codejudge/sb/service/DriverService.java
+++ src/main/java/org/codejudge/sb/service/DriverService.java
@@ -1,1 +1,20 @@
+package org.codejudge.sb.service;
 
+import java.util.List;
+
+import org.codejudge.sb.model.DriverModel;
+
+
+public interface DriverService {
+	
+	public List<DriverModel> retrieveDriverModels();
+
+	public DriverModel getDriverModel(Long driverID);
+	 
+	public void saveDriverModel(DriverModel driverModel);
+	 
+	public void deleteDriverModel(Long driverID);
+	 
+	public void updateDriverModel(DriverModel driverModel);
+	
+}
